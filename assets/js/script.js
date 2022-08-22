850066cc996552d69459a9d4efd6b0666d41b729
Henish Mavani <henishmavani18@gmail.com>
2022-08-22 17:49:56 +0530

    slider issu solw

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,12 +36,6 @@ $(document).ready(function () {
 });
 
 
- // on click copy the content from #copy-id to clipboard
-document.querySelector("#click-to-copy").addEventListener("click", (e) => {
-  let value = document.querySelector("#copy-id");
-  value.select();
-  document.execCommand("copy");
-});
 
 
 /*********  Main PDP Slick  *********/
@@ -257,6 +251,12 @@ $(document).ready(function () {
     ]
   });
 
- 
+  // on click copy the content from #copy-id to clipboard
+document.querySelector("#click-to-copy").addEventListener("click", (e) => {
+  let value = document.querySelector("#copy-id");
+  value.select();
+  document.execCommand("copy");
+});
+
 
 });
